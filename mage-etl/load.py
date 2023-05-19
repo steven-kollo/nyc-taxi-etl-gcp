8a809331ea7f7554b499615c0f46aa61f3d33857
Steven <steven.kollo.workspace@gmail.com>
2023-05-19 15:09:29 -0400

    Create load.py

---
+++ mage-etl/load.py
@@ -0,0 +1,21 @@
+from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+@data_exporter
+def export_data_to_big_query(data, **kwargs) -> None:
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    for key, value in data.items():
+        table_id = 'uber-etl-386321.taxi_data.{}'.format(key)    
+        BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            DataFrame(value),
+            table_id,
+            if_exists='replace',  # Specify resolution policy if table name already exists
+        )
